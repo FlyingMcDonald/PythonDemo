6939754a271fc05ae7a48fea6aa8169fc4247d1a
FlyingMcDonald <1476211809@qq.com>
2019-11-27 16:48:52 +0800

    add some

--- RequestsTest/certificate.py
+++ RequestsTest/certificate.py
@@ -1,4 +1,16 @@
 import requests
+from requests.packages import urllib3
 
-response = requests.get('https://www.12306.cn')
-print(response.status_code)
+urllib3.disable_warnings()
+
+class CertificateVerify: 
+    def addVerify(self):
+        response = requests.get('https://www.12306.cn')
+        print(response.status_code)
+    def noVerify(self):
+        response = requests.get('http://www.12306.cn', verify = False)
+        print(response.status_code)
+
+requestObj = CertificateVerify()
+requestObj.noVerify()
+
